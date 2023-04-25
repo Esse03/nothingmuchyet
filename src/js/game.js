1a222abea4f075ddac541f44a703bd74ac650fc5
Esse03 <essetijdelijk@gmail.com>
2023-04-25 15:23:04 +0200

    new comment

--- src/js/game.js
+++ src/js/game.js
@@ -8,7 +8,6 @@ export class Game extends Engine {
         super({ width: 800, height: 600 })
         this.start(ResourceLoader).then(() => this.startGame())
 
-        // new comment
     }
 
     startGame() {
@@ -17,7 +16,22 @@ export class Game extends Engine {
         fish.graphics.use(Resources.Fish.toSprite())
         fish.pos = new Vector(400, 300)
         fish.vel = new Vector(-10,0)
+        fish.scale = new Vector(0.1, 0.1)
         this.add(fish)
+
+        const angler = new Actor()
+        angler.graphics.use(Resources.Fish.toSprite())
+        angler.pos = new Vector(400, 300)
+        angler.vel = new Vector(-10,0)
+        angler.scale = new Vector(0.1, 0.1)
+        this.add(angler)
+
+        const blow = new Actor()
+        blow.graphics.use(Resources.Fish.toSprite())
+        blow.pos = new Vector(400, 300)
+        blow.vel = new Vector(-10,0)
+        blow.scale = new Vector(0.1, 0.1)
+        this.add(blow)
     }
 }
 
--- src/js/resources.js
+++ src/js/resources.js
@@ -1,9 +1,13 @@
 import { ImageSource, Sound, Resource, Loader } from 'excalibur'
 import fishImage from '../images/fish.png'
+import anglerImage from '../images/angler.png'
+import blowImage from '../images/blow.png'
 
 const Resources = {
-    Fish: new ImageSource(fishImage)
+    Fish: new ImageSource(fishImage),
+    Angler: new ImageSource(anglerImage),
+    Blow: new ImageSource(blowImage)
 }
-const ResourceLoader = new Loader([Resources.Fish])
+const ResourceLoader = new Loader([Resources.Fish, Resources.Angler, Resources.Blow])
 
 export { Resources, ResourceLoader }
